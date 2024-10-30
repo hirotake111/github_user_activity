dd0a2a1f48ffe3cc118adcb85ba020252067769f
hirotake111 <hirotake111@users.noreply.github.com>
2024-10-30 17:37:51 +0900

    ocaml: update main.ml (in progress)

--- ocaml/gua/bin/main.ml
+++ ocaml/gua/bin/main.ml
@@ -1,3 +1,5 @@
+open Core
+open Async
 open Lwt
 open Cohttp
 open Cohttp_lwt_unix
@@ -7,22 +9,44 @@ let username = ref ""
 let get_username un = username := un
 let () = Arg.parse [] get_username usage_msg
 let url = "https://api.github.com/users/" ^ !username ^ "/events"
-let () = Printf.printf "url: %s\n" url
+(* let () = Printf.printf "url: %s\n" url *)
+
+let get_data_from_string s =
+  match Yojson.Safe.from_string s with
+  | `Assoc kv_list -> (
+      let find key =
+        match List.Assoc.find ~equal:String.equal kv_list key with
+        | None | Some (`String "") -> None
+        | Some s -> Some (Yojson.Safe.to_string s)
+      in
+      match find "Abstract" with Some _ as x -> x | None -> find "Definition")
+  | _ -> None
 
 let body =
-  Client.get (Uri.of_string url) >>= fun (resp, body) ->
+  let url = Uri.of_string url in
+  Client.get url >>= fun (resp, body) ->
   let code = resp |> Response.status |> Code.code_of_status in
-  Printf.printf "Response code: %d\n" code;
-  Printf.printf "Received a response from the server";
-  Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);
-  body |> Cohttp_lwt.Body.to_string >|= fun body ->
-  Printf.printf "Body of length: %d\n" (String.length body);
-  body
+  match code with
+  | 200 ->
+      Cohttp_lwt.Body.to_string body >>= fun body ->
+      (* let json = Yojson.Safe.from_string body in *)
+      let a = get_data_from_string body in
+      Lwt.return (Ok a)
+  | code ->
+      let err =
+        code |> Int.to_string |> Printf.sprintf "error with status code %s"
+      in
+      Lwt.return (Error err)
+(* Printf.printf "Response code: %d\n" code; *)
+(* Printf.printf "Received a response from the server"; *)
+(* Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string); *)
+(* body |> Cohttp_lwt.Body.to_string >|= fun body -> *)
+(* Printf.printf "Body of length: %d\n" (String.length body); *)
+(* body *)
+(* let json = Yojson.Safe.from_string body in *)
 
 let () =
   let body = Lwt_main.run body in
-  body |> String.length |> print_int;
-  print_endline "Received body\n"
+  match body with Ok _ -> print_endline "OK" | Error msg -> print_endline msg
 
-(* let () = *)
-(*   let uri = Uri.of_string url in *)
+(* body |> String.length |> Int.to_string |> Printf.printf "length: %s\n" *)
